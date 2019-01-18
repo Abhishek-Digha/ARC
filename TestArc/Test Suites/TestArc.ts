15adb634c252fdf16815956437d7cfb25e7a49a8
Abhishek-Digha <abhishek.digha1990@gmail.com>
2019-01-19 00:06:34 +0530

    arc

---
+++ TestArc/Test Suites/TestArc.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestArc</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8312e2bb-4e8e-436a-930d-86c3f0c766ac</testSuiteGuid>
+   <testCaseLink>
+      <guid>eeb745ea-cfbe-44f7-9f57-32464077400a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginArc</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
